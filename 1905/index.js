5bb90e261c88ea43c9372baee8a3cbd7c544ebdf
Lakhwinder22 <103108045+Lakhwinder22@users.noreply.github.com>
2022-05-25 16:54:54 +0530

    Day19 Java Uploaded

---
+++ 1905/index.js
@@ -0,0 +1,11 @@
+// Jquery = JS library which provides effective method for doing something in fronted and gives essential features 
+// like DON manipulation, AJAX interaction and so  on...
+
+$("button").click(function(){
+    // $("p").hide();
+    // $("p").toggle();
+    $("#learn").toggle();
+})
+
+
+
