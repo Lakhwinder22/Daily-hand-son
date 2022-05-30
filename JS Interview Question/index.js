eda19eb3480875072450185bb3158b2a60f01b8b
Lakhwinder22 <103108045+Lakhwinder22@users.noreply.github.com>
2022-05-30 20:11:32 +0530

    Java Interview Day1

---
+++ JS Interview Question/index.js
@@ -0,0 +1,39 @@
+// 1. Difference between “ == “ and “ === “ operators?
+
+// == it compare the value of variables,not data type
+// === it compare the value and data type of 2 variables
+
+// What is the spread operator?
+
+// Its used in the variable array where there is more than 1 values are expected. 
+// it allows us the privilege to obtain a list of parameters from an array.
+
+// What are the differences between var, let and const?
+
+// let - 1.They can be declared in a line and then intialized with a value in the another lines.
+    //   2.They can be also declared and intialized in the same line.
+    //   3. They cannot have be also redeclared But can be reintialized.
+    //   4. We cannot have Two variable with same name in Let.
+
+// Const - 1. They can be declared in a line and then intialized with a value in the same line only.
+    //     2. They cannot be redeclared.
+    //     3. But const  cannot be reinitialized also.
+    //     4. We cannot have Two  variable with same name in const.
+
+// Var - 1.They can be declared in a line and then intialized with a value in the another lines.
+    //   2.They can be also declared and intialized in the same line.
+    //   3. They cannot have be also redeclared But can be reintialized.
+    //   4. We can have Two or more variables with same name in Var.
+
+// What is execution context?
+
+// In the First stage Memory Allocation all the variables will get memory but will not be initialized.
+
+// What is meant by first class functions?
+
+// A programming language is said to have First - Class functions when functions in that language are treated like any other variable. 
+
+// What are closures?
+
+// A closures is the combination of a function bundled together (enclosed) with refernces to its surrounding state.
+//  In other words a closure gives you access to an outer function's scope from an inner function
